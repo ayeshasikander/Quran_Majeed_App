19708de7bc30891b6db4c9e42a395de27802e347
ayeshasikander <sikanderayesha46@gmail.com>
2024-01-19 15:34:44 +0500

    on working

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let audio = document.querySelector(".audioPlayer"),
     prev = document.querySelector(".prev");
 getSurahs();
 allSurahs = document.querySelectorAll(".surahs div")
-console.log(allSurahs)
 function getSurahs() {
     fetch("https://api.quran.gading.dev/surah")
         .then((response) => response.json())
@@ -30,10 +29,69 @@ function getSurahs() {
                         .then(response => response.json())
                         .then(data => {
                             let verses = data.data.verses;
+                            AyahAudios = [];
+                            AyahText = [];
                             verses.forEach(verse => {
-                                console.log(verse.text.arab); 
+                                AyahAudios.push(verse.audio.primary);
+                                AyahText.push(verse.text.arab);
                             })
-                             
+                            let ayahIndex = 0;
+                            updateAyah(ayahIndex);
+                            audio.addEventListener("ended", () => {
+                                ayahIndex++;
+                                if (ayahIndex < AyahAudios.length) {
+                                    updateAyah(ayahIndex);
+                                } else {
+                                    ayahIndex = 0;
+                                    updateAyah(ayahIndex);
+                                    Swal.fire({
+                                        icon: "success",
+                                        title: "Sadaqallahul Azim – ﺻﺪﻕ ﺍﻟﻠﻪ ﺍﻟﻌﻈﻴﻢ",
+                                        showConfirmButton: true,
+                                        timer: 1500
+                                    });
+                                    isplay = false;
+                                    playpause();
+
+                                }
+                            })
+
+                            next.addEventListener("click", () => {
+
+                                ayahIndex < AyahAudios.length - 1 ? ayahIndex++ : ayahIndex = 0;
+                                updateAyah(ayahIndex);
+                            });
+                            prev.addEventListener("click", () => {
+                                ayahIndex == 0 ? ayahIndex = AyahAudios.length - 1 : ayahIndex--;
+                                updateAyah(ayahIndex);
+
+                            });
+                            let isplay = false;
+                            playpause();
+                           
+                            function playpause() {
+                                if (isplay) {
+                                    audio.pause();
+                                    play.innerHTML = '<i class="bi bi-play-fill"></i>';
+                                    isplay = false;
+                                } else {
+                                    audio.play().catch((error) => {
+                                        console.error("Error playing audio:", error);
+                                    });
+                                    play.innerHTML = '<i class="bi bi-pause-fill"></i>';
+                                    isplay = true;
+                                }
+                            }
+                            play.addEventListener("click", playpause);
+
+                            function updateAyah(index) {
+                                audio.src = AyahAudios[index];
+                                ayah.innerHTML = AyahText[index];
+                                return audio.play().catch((error) => {
+                                    console.error("Error playing audio:", error);
+                                });
+                            }
+
                         })
                 })
             })
